932b1274365fdc6e8c7edddd04e2444a94c3c42f
ginger-ninja04 <alexander.price.2@mail.weber.edu>
2025-09-24 11:06:51 -0600

    ADD: Added clear function and ask for player name, stored as 'name'

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
+# create clear
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear()
+
+#Ask for player name
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
-print("Hello World")
+name = input("Whats your name, adventurer? ")
+print("Hello,",name)
+
+print(f"Welcome, {name}! Your journey begins here...")
